e5e2bc3ca5b32669772eedba2673d289b99cf5b2
theos-the-dawg <motheo13@outlook.com>
2026-08-03 11:24:10 +0200

    prac-init

---
+++ index.js
@@ -0,0 +1,35 @@
+// ///array   
+// const array = ['a',1,{"name":"motheo"}];
+// const array2D = [[1,2,3],{"name":"master"}];
+// const array_index = array2D.at(0)
+
+// console.log(array_index[0]);
+
+// //object
+// const money = 300;
+// const person = {"name":"Gerald","surname":"K","interest":function apply_interest(){
+//     const interest_value =  10/100;
+//     const total_interest = money * 10 + interest_value 
+
+// }}
+
+// //Loops
+
+ const values = [1,2,3,4,5,6,7,7,7,8,9,10];
+ for(let index=0;index<values.length;index++){
+     const indexes = values[index];
+     console.log(indexes)
+     if(indexes === 7 ){
+        const count_7 = [index];
+ }
+}
+values.forEach(element => {
+    
+});
+console.log(values);
+
+const ages = [15,20,30,15,15];
+
+    
+    
+
