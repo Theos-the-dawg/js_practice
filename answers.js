5b93700a9478f374c1215dafa2ab07d5f53591df
valencycodecussler <geraldkapurura07@gmail.com>
2026-08-03 13:54:15 +0200

    git init

---
+++ answers.js
@@ -0,0 +1,17 @@
+
+//1. 1)Create a js function that for loops through an array and show the first and last index of the array.
+function ForLoopArray(params) {
+    
+    for (let index = 0; index < array.length; index++) {
+        if (i === 0 ) {
+            console.log("First inedx:"+ i);
+        }
+        if(i === array.length-1){
+            console.log("Last index:" + i)
+        }
+        
+    }
+    
+}
+const array = [10,20,30,40,50]
+console.log(ForLoopArray(array));
