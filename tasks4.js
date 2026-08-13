df5192f380518c463f74ce76b67014dec1df872d
massomaster24 <massomaster24@gmail.com>
2026-08-13 13:40:09 +0200

    13082026

---
+++ tasks4.js
@@ -0,0 +1,36 @@
+// Execise 1
+// let num1 = 20;
+// let num2 = 6;
+// let sum = num1 + num2;
+// console.log(`Sum: ${sum} `)
+// let difference = num1 - num2;
+// console.log(`difference: ${difference}`)
+// let product = num1*num2;
+// console.log(`product:${product}`)
+// let quotient = num1 / num2
+// console.log(`quotient :${quotient}`) 
+// let remainder = num1 % num2;
+// console.log(`remainder :${remainder}`)
+
+// Exercise 2: area of a rectangle
+// let length = 12;
+// let width = 8;
+// const rectangle(length*width,
+// area = length*width
+// console.log(area))
+
+
+// //Excercise 3
+const celcius
+// let celsius = 30;
+// F = (celsius*1.8) + 32
+// console.log(celsius)
+
+// Exercise 4: Circle area
+// let radius = 5;
+// area = 3.142 *( radius * 2)
+// console.log(area)
+
+// Exercise 5: power calculator
+// const Math.pow(7,3)
+// console.log(Math.pow(7,3));
